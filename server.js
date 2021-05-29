0b6099a17468574d10d2bd207374257daf208fcf
greaterheightscomputer <greaterheightscomputer@gmail.com>
2021-05-29 22:09:58 +0100

    Posting with Postman

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-
+const bodyParser = require('body-parser');
 // app.get('/', (req, res)=>{
 //   res.send('Hello Web 2.0');
 // })
@@ -8,6 +8,9 @@ const app = express();
 // app.listen(3000, ()=>{
 //   console.log('server is running on port 3000');
 // })
+
+//its inform the post request api or other api that we are passing data value either from postman or any other medium
+app.use(bodyParser.json());
 
 app.post('/', (req, res)=>{
   var email = req.body.email;
